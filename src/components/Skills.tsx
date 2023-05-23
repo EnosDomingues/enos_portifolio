0f53c9e9ab516b683066f5b88e54df5f62af0911
Enos Domingues <enosdomingues@gmail.com>
2023-05-23 22:27:52 +0200

    feat: Skills

---
+++ src/components/Skills.tsx
@@ -0,0 +1,80 @@
+export function Skills() {
+  return (
+    <section className="flex flex-col mt-56">
+      <h2 className="uppercase font-medium text-6xl tracking-widest">Skills</h2>
+      <div className="pl-[20%] pt-[10%] PR-[5%]">
+        <ul>
+          <li>
+            <span className="uppercase font-bold py-1 border-b border-gray-900">
+              JavaScript
+            </span>
+            <p className="text-xl py-2">
+              Strong command over JavaScript, the fundamental programming
+              language for web development.
+            </p>
+          </li>
+
+          <li className="mt-4">
+            <span className="uppercase font-bold py-1 border-b border-gray-900">
+              TypeScript
+            </span>
+            <p className="text-xl py-2">
+              A statically typed superset of JavaScript, enabling safer and more
+              efficient development.
+            </p>
+          </li>
+
+          <li className="mt-4">
+            <span className="uppercase font-bold py-1 border-b border-gray-900">
+              Next.JS
+            </span>
+            <p className="text-xl py-2">
+              A popular React framework for building server-rendered and
+              statically generated websites.
+            </p>
+          </li>
+
+          <li className="mt-4">
+            <span className="uppercase font-bold py-1 border-b border-gray-900">
+              React
+            </span>
+            <p className="text-xl py-2">
+              A powerful JavaScript library for building user interfaces,
+              enabling reusable and component-based development.
+            </p>
+          </li>
+
+          <li className="mt-4">
+            <span className="uppercase font-bold py-1 border-b border-gray-900">
+              Node.JS
+            </span>
+            <p className="text-xl py-2">
+              A server-side JavaScript runtime environment, for building
+              scalable and efficient back-end applications.
+            </p>
+          </li>
+
+          <li className="mt-4">
+            <span className="uppercase font-bold py-1 border-b border-gray-900">
+              React Native
+            </span>
+            <p className="text-xl py-2">
+              A framework for building cross-platform mobile applications using
+              JavaScript and React.
+            </p>
+          </li>
+
+          <li className="mt-4">
+            <span className="uppercase font-bold py-1 border-b border-gray-900">
+              Tailwind CSS
+            </span>
+            <p className="text-xl py-2">
+              A utility-first CSS framework, enabling rapid and responsive web
+              development with pre-built classes.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
+}
